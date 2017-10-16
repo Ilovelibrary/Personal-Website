934f4069ce709800ae89e2fb288ab5760b05fc3a
Pu <p5gao@ucsd.edu>
2017-10-15 19:17:01 -0700

    remove contact

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { flyInOut} from '../animations/app.animation';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 export class ProjectsComponent implements OnInit {
 
